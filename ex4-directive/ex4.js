bc4950040c4773d2ee4c9806ecb3162b9760d08b
Ian Kelly <iandotkelly@gmail.com>
2014-03-08 18:45:44 -0500

    Add 4th example

---
+++ ex4-directive/ex4.js
@@ -0,0 +1,37 @@
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myListController', function ($scope) {
+
+	$scope.myCollection = [
+		{
+			name: 'Ian',
+			address: '123 Main St, Ann Arbor MI'
+		},
+		{
+			name: 'Harry Potter',
+			address: 'Hogwarts'
+		},
+		{
+			name: 'Elizabeth',
+			address: 'Buckingham Palace, London, UK'
+		}
+	];
+
+});
+
+myApp.directive('addressWidget', function ($compile) {
+
+	return {
+		scope: {
+			person: '=person'
+		},
+		restrict: 'E',
+		template: '<div class="outline">' +
+			'   <div class="name">{{person.name}}</div>' +
+			'   <div class="address">{{person.address}}</div>' +
+			'	<div ng-show="person.name === \'Ian\'">Hello Ian</div>' +
+			'</div>'
+	};
+
+});
